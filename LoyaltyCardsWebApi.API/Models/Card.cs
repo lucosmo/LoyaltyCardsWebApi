b5f53e38c8959eb915645f212f3752feb3a0f215
lucosmo <lukasz.kosmaczewski@gmail.com>
2024-08-07 20:34:09 +0100

    updated Card and User

--- LoyaltyCardsWebApi.API/Models/Card.cs
+++ LoyaltyCardsWebApi.API/Models/Card.cs
@@ -1,7 +1,10 @@
+namespace LoyalityCardsWebApi.API.Models;
+
 public class Card 
 {
     public int Id { get; set;}
     public required string Name { get; set;}
-    public int Image { get; set;}
-    public int Code { get; set;}
+    public required string Image { get; set;}
+    public required string Barcode { get; set;}
+    public DateTime AddedAt { get; set;}
 }
--- LoyaltyCardsWebApi.API/Models/User.cs
+++ LoyaltyCardsWebApi.API/Models/User.cs
@@ -1,9 +1,13 @@
+namespace LoyalityCardsWebApi.API.Models;
 public class User
 {
     public long Id { get; set;}
     public required string UserName { get; set;}
+    public required string Password { get; set;}
+    public required string Email { get; set;}
     public string? FirstName { get; set;}
     public string? LastName { get; set;}
     public DateTime AccountCreatedDate { get; set;}
-    public List<Card>? Cards; 
+    public List<Card>? Cards {get; set;}
+    public string? Settings { get; set;}
 }
