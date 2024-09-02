ea7c24424f015b14af3f272f39b35f729ede06a0
lucosmo <lukasz.kosmaczewski@gmail.com>
2024-09-02 16:06:23 +0100

    Added CreateUser DTO

---
+++ LoyaltyCardsWebApi.API/Data/DTOs/CreateUserDto.cs
@@ -0,0 +1,7 @@
+namespace LoyalityCardsWebApi.API.Data.DTOs;
+public class CreateUserDto
+    {
+        public required string UserName { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
