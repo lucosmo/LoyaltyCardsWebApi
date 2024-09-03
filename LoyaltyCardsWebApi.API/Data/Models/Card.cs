bcb3a50f235bfbeacc531dbbaffff32d0134e2c4
lucosmo <lukasz.kosmaczewski@gmail.com>
2024-09-03 18:59:50 +0100

    updated user & card models -added navigation property to card model

--- LoyaltyCardsWebApi.API/Data/Models/Card.cs
+++ LoyaltyCardsWebApi.API/Data/Models/Card.cs
@@ -7,19 +7,23 @@ public class Card
     public required string Image { get; set;}
     public required string Barcode { get; set;}
     public DateTime AddedAt { get; set;}
+    public int UserId { get; set; }
+    public required User User { get; set; }
 
     public Card()
     {
         
     }
 
-    public Card(int id, string name, string image, string barcode, DateTime addedAt)
+    public Card(int id, string name, string image, string barcode, DateTime addedAt, int userId, User user)
     {
         Id = id;
         Name = name;
         Image = image;
         Barcode = barcode;
         AddedAt = addedAt;
+        UserId = userId;
+        User = user;
     }
 }
 
