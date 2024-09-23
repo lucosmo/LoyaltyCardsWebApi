a687d32d2a6182309b978961e07daf1f9c8b7d61
lucosmo <lukasz.kosmaczewski@gmail.com>
2024-09-23 20:10:52 +0100

    added annotations (email, password) for CreateUserDto

--- LoyaltyCardsWebApi.API/Data/DTOs/CreateUserDto.cs
+++ LoyaltyCardsWebApi.API/Data/DTOs/CreateUserDto.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LoyalityCardsWebApi.API.Data.DTOs;
 public class CreateUserDto
     {
         public required string UserName { get; set; }
+        [EmailAddress(ErrorMessage = "Invalid email address format")]
         public required string Email { get; set; }
+        [MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
         public required string Password { get; set; }
     }
