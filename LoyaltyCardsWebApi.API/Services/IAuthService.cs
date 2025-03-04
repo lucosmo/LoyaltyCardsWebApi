cbbb4df264db54ad54051e8e8ad74088a86f1506
lucosmo <lukasz.kosmaczewski@gmail.com>
2025-03-04 17:40:47 +0000

    update AuthService to Login, receive auth related data abour user (token, token expiry date, userid), add marking token as revoke token

--- LoyaltyCardsWebApi.API/Services/IAuthService.cs
+++ LoyaltyCardsWebApi.API/Services/IAuthService.cs
@@ -3,5 +3,11 @@ using LoyalityCardsWebApi.API.Data.DTOs;
 namespace LoyaltyCardsWebApi.API.Services;
 public interface IAuthService
 {
-    Task<string> LoginAsync(LoginDto loginDto);
+    Task<Result<string>> LoginAsync(LoginDto loginDto);
+    Task<Result<string>> RegisterAsync(CreateUserDto newUserDto);
+    Result<string> GetTokenAuthHeader();
+    Result<int> GetUserId();
+    Result<DateTime> GetTokenExpiryDate();
+    Task<Result<string>> AddRevokedTokenAsync(string token, DateTime expiryDate, int userId);
+
 }
