b3d82cd29ac997071aaf33b0d9d3c38f7f9587dc
lucosmo <lukasz.kosmaczewski@gmail.com>
2025-07-22 23:10:51 +0100

    create DTOs for Card and change navigation property within class Card to nullable, so don't need to provide it while creating instance of Card

---
+++ LoyaltyCardsWebApi.API/Data/DTOs/CardDto.cs
@@ -0,0 +1,9 @@
+﻿namespace LoyaltyCardsWebApi.API.Data.DTOs
+{
+    public class CardDto
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required int UserId { get; set; }
+    }
+}
---
+++ LoyaltyCardsWebApi.API/Data/DTOs/UpdatedCardDto.cs
@@ -0,0 +1,9 @@
+﻿namespace LoyaltyCardsWebApi.API.Data.DTOs
+{
+    public class UpdatedCardDto
+    {
+        public required string Name { get; set; }
+        public string Image { get; set; } = "https://..default-image.png"; // Default image URL
+        public required string Barcode { get; set; }
+    }
+}
