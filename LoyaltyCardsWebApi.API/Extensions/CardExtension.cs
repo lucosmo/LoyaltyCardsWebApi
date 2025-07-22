6a2f28877b9572820e57de3aa880daeeb613cbb3
lucosmo <lukasz.kosmaczewski@gmail.com>
2025-07-22 23:12:11 +0100

    add extension method to Card class used for creating CardDto from Card instance

---
+++ LoyaltyCardsWebApi.API/Extensions/CardExtension.cs
@@ -0,0 +1,18 @@
+﻿using LoyaltyCardsWebApi.API.Data.DTOs;
+using LoyaltyCardsWebApi.API.Models;
+
+namespace LoyaltyCardsWebApi.API.Extensions
+{
+    public static class CardExtension
+    {
+        public static CardDto ToDto(this Card card)
+        {
+            return new CardDto
+            {
+                Id = card.Id,
+                Name = card.Name,
+                UserId = card.UserId
+            };
+        }
+    }
+}
