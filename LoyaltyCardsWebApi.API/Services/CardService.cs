5ff37f82a08cb189fb645668cd93a0a7f3a04483
lucosmo <lukasz.kosmaczewski@gmail.com>
2025-07-31 14:39:24 +0100

    implement delete, get card, update card functionality within CardService

--- LoyaltyCardsWebApi.API/Services/CardService.cs
+++ LoyaltyCardsWebApi.API/Services/CardService.cs
@@ -32,17 +32,51 @@ namespace LoyaltyCardsWebApi.API.Services
             };
 
             var createdCard = await _cardRepository.CreateCardAsync(newCardModel);
+            
+            if (createdCard is null)
+            {
+                return Result<CardDto>.Fail("Card creation failed.");
+            }
+            
             return Result<CardDto>.Ok(createdCard.ToDto());
         }
 
-        public Task<Result<Card>> DeleteCardAsync(int id)
+        public async Task<Result<CardDto>> DeleteCardAsync(int id)
         {
-            throw new NotImplementedException();
+            var currentUserResult = await _userService.GetCurrentUserAsync();
+            var cardDeleted = await _cardRepository.Delete(id);
+            if (cardDeleted is null)
+            {
+                return Result<CardDto>.Fail("Card not found or deletion failed.");
+            }
+            else if(!currentUserResult.Success || currentUserResult.Value is null || cardDeleted.Id != currentUserResult.Value.Id)
+            {
+                return Result<CardDto>.Fail("You do not have permission to delete this card.");
+            }
+            else
+            {
+                return Result<CardDto>.Ok(cardDeleted.ToDto());
+            }
         }
 
-        public Task<Result<Card>> GetCardByIdAsync(int id)
+        public async Task<Result<CardDto>> GetCardByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            var cardResult = await _cardRepository.GetCardByIdAsync(id);
+            var currentUserResult = await _userService.GetCurrentUserAsync();
+
+            if (cardResult is null)
+            {
+                return Result<CardDto>.Fail("Card not found");
+            }
+            if (!currentUserResult.Success || currentUserResult.Value is null)
+            {
+                return Result<CardDto>.Fail(currentUserResult.Error ?? "User not found");
+            }
+            if (cardResult.UserId != currentUserResult.Value.Id)
+            {
+                return Result<CardDto>.Fail("You do not have permission to access this card.");
+            }
+            return Result<CardDto>.Ok(cardResult.ToDto());
         }
 
         public async Task<Result<IEnumerable<CardDto>>> GetCardsByUserIdAsync(int userId)
@@ -55,9 +89,31 @@ namespace LoyaltyCardsWebApi.API.Services
             return Result<IEnumerable<CardDto>>.Ok(cards.Select(card => card.ToDto()));
         }
 
-        public Task<Result<CardDto>> UpdateCardAsync(int id, UpdatedCardDto card)
+        public async Task<Result<CardDto>> UpdateCardAsync(int id, UpdateCardDto updateCard)
         {
-            throw new NotImplementedException();
+            var currentCard = await _cardRepository.GetCardByIdAsync(id);
+            if (currentCard is null)
+            {
+                return Result<CardDto>.Fail("Card not found.");
+            }
+            
+            Card card = new Card
+            {
+                Id = id,
+                Name = updateCard.Name ?? currentCard.Name,
+                Image = updateCard.Image ?? currentCard.Image,
+                Barcode = updateCard.Barcode ?? currentCard.Barcode,
+                UserId = currentCard.UserId, 
+                AddedAt = currentCard.AddedAt 
+            };
+
+            var updatedCardResult = await _cardRepository.UpdateCardAsync(card);
+            if (updatedCardResult is null)
+            {
+                return Result<CardDto>.Fail("Card not found or update failed.");
+            }
+            return Result<CardDto>.Ok(updatedCardResult.ToDto());
+
         }
     }
 }
