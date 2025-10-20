b845bb6b960503d82a54bf5b526a57acf4d24cb8
lucosmo <lukasz.kosmaczewski@gmail.com>
2025-10-20 10:34:45 +0100

    resolve PR comments

--- LoyaltyCardsWebApi.API/Attributes/ValidateNotEmptyIfProvided.cs
+++ LoyaltyCardsWebApi.API/Attributes/ValidateNotEmptyIfProvided.cs
@@ -10,7 +10,11 @@ public class ValidateNotEmptyIfProvided : ValidationAttribute
         {
             return ValidationResult.Success;
         }
-        if (string.IsNullOrWhiteSpace((string)value))
+        if (value is not string)
+        {
+            throw new ValidationException($"{validationContext.DisplayName} must be a string.");
+        }
+        if (value is string strValue && string.IsNullOrWhiteSpace(strValue))
         {
             return new ValidationResult($"{validationContext.DisplayName} can't be empty or whitespace.");
         }
