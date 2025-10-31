1a4962fd4b14327be249ebb27060e562fdfa6572
lucosmo <lukasz.kosmaczewski@gmail.com>
2025-10-31 15:17:34 +0000

    add NoContent SuccessType and Result.NoContent() factory for 204 HTTP responses

--- LoyaltyCardsWebApi.API/Common/Result.cs
+++ LoyaltyCardsWebApi.API/Common/Result.cs
@@ -20,6 +20,7 @@ public class Result<T>
 
     public static Result<T> Ok(T value) => new(true, SuccessTypes.Ok, value, string.Empty, ErrorTypes.None);
     public static Result<T> Created(T value, string location) => new(true, SuccessTypes.Created, value, string.Empty, ErrorTypes.None, location);
+    public static Result<T> NoContent() => new(true, SuccessTypes.NoContent, default, string.Empty, ErrorTypes.None);
     public static Result<T> NotFound(string error) => new(false, null, default, error, ErrorTypes.NotFound);
     public static Result<T> BadRequest(string error) => new(false, null, default, error, ErrorTypes.BadRequest);
     public static Result<T> Unauthorized(string error) => new(false, null, default, error, ErrorTypes.Unauthorized);
--- LoyaltyCardsWebApi.API/Common/SuccessTypes.cs
+++ LoyaltyCardsWebApi.API/Common/SuccessTypes.cs
@@ -3,5 +3,6 @@ namespace LoyaltyCardsWebApi.API.Common;
 public enum SuccessTypes
 {
     Ok,
-    Created
+    Created,
+    NoContent
 }
